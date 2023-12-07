cadf342f38dc285d32d855b5b6f83512eb917061
OlgaKuvs <olja_kuv@inbox.lv>
2023-12-07 22:33:17 +0000

    feat: create products template and functionality to display all products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product, Category
 
-# Create your views here.
+
+def all_products(request):
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    context = {
+        "products": products,
+        'categories': categories,      
+    }
+
+    return render(request, "products/products.html", context)
+
--- t_studio/urls.py
+++ t_studio/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('core.urls')),
+    path('products/', include('products.urls')),   
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
