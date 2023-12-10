4c7eed7ea35db27b2b98e8a8dcbfa194d3d24897
OlgaKuvs <olja_kuv@inbox.lv>
2023-12-10 19:17:37 +0000

    feat: add cart function to add products

--- cart/contexts.py
+++ cart/contexts.py
@@ -3,16 +3,14 @@ from decimal import Decimal
 def cart_contents(request):
 
     cart_items = []
-    total = 10
+    total = 0
     weight_total = 0
     product_count = 0
     cart = request.session.get('cart', {})
     
     delivery = total * Decimal()
     grand_total = delivery + total
-
-    print(grand_total)
-    
+      
     context = {
         'cart_items': cart_items,
         'total': total,
