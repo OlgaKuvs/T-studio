54be6689a8bd78b9db34489dddb3abd760adcf28
OlgaKuvs <olja_kuv@inbox.lv>
2023-12-12 21:32:59 +0000

    feat: add adjust item quantity functionality to cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -23,22 +23,22 @@ def cart_contents(request):
             'quantity': quantity,
             'product': product,         
         })
-    print("ttt", total_weight)
+    # print("ttt", total_weight)
 
     """Get the shipping cost from the database for 
     the current parcel weight + 100g per packaging """
 
     all_rates= Shipping.objects.order_by('order_weight')
-    print("all_weights", all_rates)
+    # print("all_weights", all_rates)
     for r in all_rates:        
         if r.order_weight > total_weight + 100:
             parcel_weight = r.order_weight                   
-            print("PPP", parcel_weight)
+            # print("PPP", parcel_weight)
             break                 
            
     shipping_cost = Shipping.objects.filter(order_weight=parcel_weight)\
                     .values_list('postal_rates').first()[0]
-    print("SSS", shipping_cost) 
+    # print("SSS", shipping_cost) 
 
     grand_total = shipping_cost + total
     # print(cart_items)
--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import (render, redirect, 
+    reverse, get_object_or_404)
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
 def view_cart(request):
-    """ A view that renders the cart contents page """
+    """ A view that renders the cart contents page """  
     return render(request, 'cart/cart.html')
 
 
@@ -20,5 +23,27 @@ def add_to_cart(request, item_id):
         cart[item_id] = quantity
 
     request.session['cart'] = cart
-    print(request.session['cart'])
+    # print(request.session['cart'])
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))  
+    cart = request.session.get('cart', {})
+    
+    if quantity > 0:
+        cart[item_id] = quantity
+        messages.success(request,
+                        (f'Updated {product.name} '
+                        f'quantity to {cart[item_id]}'))
+    else:
+        cart.pop(item_id)
+        messages.success(request,
+                            (f'Removed {product.name} '
+                            f'from your cart'))
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
