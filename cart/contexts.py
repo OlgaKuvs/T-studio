c0ab40a2f174404b8557052f7a9820f4bc9460f5
OlgaKuvs <olja_kuv@inbox.lv>
2023-12-12 17:17:40 +0000

    feat: add functionality to calculate shipping costs depending on parcel weight

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,36 +1,53 @@
 from decimal import Decimal
 from django.shortcuts import get_object_or_404
 from products.models import Product
+from .models import Shipping
+
 
 def cart_contents(request):
 
     cart_items = []
-    total = 0
-    # weight_total = 0
+    total = 0   
     product_count = 0
-    delivery = 0
+    total_weight = 0  
     cart = request.session.get('cart', {})
 
     for item_id, quantity in cart.items():
         product = get_object_or_404(Product, pk=item_id)
         total += quantity * product.price
         product_count += quantity
+        line_items_weight = quantity * product.weight        
+        total_weight += line_items_weight    
         cart_items.append({
             'item_id': item_id,
             'quantity': quantity,
             'product': product,         
         })
-    
-    grand_total = delivery + total
+    print("ttt", total_weight)
 
+    """Get the shipping cost from the database for 
+    the current parcel weight + 100g per packaging """
+
+    all_rates= Shipping.objects.order_by('order_weight')
+    print("all_weights", all_rates)
+    for r in all_rates:        
+        if r.order_weight > total_weight + 100:
+            parcel_weight = r.order_weight                   
+            print("PPP", parcel_weight)
+            break                 
+           
+    shipping_cost = Shipping.objects.filter(order_weight=parcel_weight)\
+                    .values_list('postal_rates').first()[0]
+    print("SSS", shipping_cost) 
+
+    grand_total = shipping_cost + total
     # print(cart_items)
       
     context = {
         'cart_items': cart_items,
         'total': total,
-        'product_count': product_count,
-        # 'weight_total': weight_total,
-        'grand_total': grand_total,       
+        'product_count': product_count,      
+        'grand_total': grand_total,
+        'shipping_cost': shipping_cost,         
     }
-
     return context
