5c27fa3b444881ab733ce3b2e40c551ae2fb8250
OlgaKuvs <olja_kuv@inbox.lv>
2023-12-12 22:07:11 +0000

    feat: add function to delete item from cart

--- cart/urls.py
+++ cart/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.view_cart, name='view_cart'),
     path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
     path('adjust_cart/<item_id>/', views.adjust_cart, name='adjust_cart'),
+    path('delete/<item_id>/', views.delete_from_cart, name='delete_from_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import (render, redirect, 
-    reverse, get_object_or_404)
+    reverse, get_object_or_404, HttpResponse)
 from django.contrib import messages
 from products.models import Product
 
@@ -47,3 +47,19 @@ def adjust_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+
+
+def delete_from_cart(request, item_id):
+    """Delete the item from the shopping cart"""
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)       
+        cart = request.session.get('cart', {})  
+        cart.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your cart')
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
