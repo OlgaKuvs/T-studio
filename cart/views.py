1ed81a0a674e761aa8c17321f8c7d7fc03647de9
OlgaKuvs <olja_kuv@inbox.lv>
2023-12-13 17:58:44 +0000

    feat: add cart information to message box, style messages

--- cart/views.py
+++ cart/views.py
@@ -13,14 +13,17 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
     else:
         cart[item_id] = quantity
+        messages.success(request, f'{product.name} is added to your bag')
 
     request.session['cart'] = cart
     # print(request.session['cart'])
