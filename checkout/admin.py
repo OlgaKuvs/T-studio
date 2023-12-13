8b22e1cd74cde36fed839cfd114bbf49027a2778
OlgaKuvs <olja_kuv@inbox.lv>
2023-12-13 21:12:32 +0000

    feat: add methods to models, signals and form to checkout

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total', 'lineitem_weight',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
+                       'shipping_cost', 'order_total',
+                       'grand_total',   'original_bag',
+                       'stripe_pid', 'order_weight',       
+                       )
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'city', 'street_address1',
+              'street_address2', 'county', 'shipping_cost',
+              'order_total', 'grand_total', 'original_bag',
+              'stripe_pid', 'order_weight',
+              )
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'shipping_cost',
+                    'grand_total',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -1,5 +1,10 @@
+import uuid
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
 from products.models import Product
+from cart.models import Shipping
 
 
 class Order(models.Model):
@@ -37,7 +42,7 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(default='Ireland', null=False, blank=False)
+    country = models.CharField(max_length=50, default='Ireland', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
@@ -49,7 +54,51 @@ class Order(models.Model):
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
-    weight = models.IntegerField(null=True, blank=True)
+    order_weight = models.IntegerField(null=True, blank=True)
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+    
+    def update_total(self):
+        """
+        Update grand total each time a line item is added,
+        accounting for delivery costs.
+        """
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        self.order_weight = self.lineitems.aggregate(
+            Sum('lineitem_weight'))['lineitem_weight__sum'] or 0
+        
+        all_rates= Shipping.objects.order_by('order_weight')
+        for r in all_rates:        
+            if r.order_weight > self.order_weight + 100:
+                parcel_weight = r.order_weight  
+                break
+        self.shipping_cost = Shipping.objects.filter(order_weight=parcel_weight)\
+                    .values_list('postal_rates').first()[0]                
+        
+        # if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+        #     sdp = settings.STANDARD_DELIVERY_PERCENTAGE
+        #     self.delivery_cost = self.order_total * sdp / 100
+        # else:
+        #     self.delivery_cost = 0
+        self.grand_total = self.order_total + self.shipping_cost 
+        self.save()
+    
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
 
 
 class OrderLineItem(models.Model):
@@ -62,3 +111,18 @@ class OrderLineItem(models.Model):
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
                                          null=False, blank=False,
                                          editable=False)
+    lineitem_weight = models.IntegerField(null=False, blank=False,
+                                         editable=False)
+    
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        self.lineitem_weight = self.product.weight * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
+    
