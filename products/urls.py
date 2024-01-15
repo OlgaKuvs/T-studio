fd1ae41ab786e18118cb8e1cedce9ea6216def65
OlgaKuvs <olja_kuv@inbox.lv>
2024-01-15 17:11:55 +0000

    feat: add functionality to edit product for site admin

--- products/urls.py
+++ products/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('category/<str:slug>/', CategoryView, name='category'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>', views.edit_product, name='edit_product'),
     path('review_product/<int:product_id>/<int:order_id>/', ReviewView.as_view(), name='review_product'),   
 ]
 
