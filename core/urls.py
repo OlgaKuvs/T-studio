b1291c2a65d20d9254092ede10a55f6c0dba12e7
OlgaKuvs <olja_kuv@inbox.lv>
2024-01-24 16:24:54 +0000

    feat: create and style electrical stimulation page

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='core'),
-    path('thai_massage', views.thai_massage, name='thai_massage'), 
+    path('thai_massage', views.thai_massage, name='thai_massage'),
+    path('ed_stimulation', views.ed_stimulation, name='ed_stimulation'), 
 ]
--- core/views.py
+++ core/views.py
@@ -6,6 +6,12 @@ def index(request):
     """ A view to return the index page """
     return render(request, 'core/index.html')
 
+
 def thai_massage(request):
     """ A view to return the thai_massage page """
     return render(request, 'core/thai_massage.html')
+
+
+def ed_stimulation(request):
+    """ A view to return the electro dinamical stimulation page """
+    return render(request, 'core/ed_stimulation.html')
